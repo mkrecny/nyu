28e9f4bd124c05b1946324db9415873ef4c12ee5
mkrecny <mkrecny@gmail.com>
2012-09-11 14:57:22 -0400

    diyhealth week 1

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -3,6 +3,29 @@
  * GET home page.
  */
 
+//render all classes
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', {
+    contentPartial : 'classes',
+    classname: false,
+    week:false
+  });
+};
+
+//render all weeks of a class
+exports.class = function(req, res){
+  res.render('index', {
+    contentPartial :'class_index',
+    classname: req.params.class,
+    week: false
+  });
+};
+
+//render a specific week
+exports.week = function(req, res){
+  res.render('index', {
+    contentPartial :'class_'+req.params.class+'_'+req.params.week,
+    classname: req.params.class,
+    week: req.params.week 
+  });
 };
